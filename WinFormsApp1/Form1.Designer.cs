f703ac8ab645e95ff2b29ccde5493325ed48c231
yhc1209 <82189968+yhc1209@users.noreply.github.com>
2021-06-06 23:03:59 +0800

    Add files via upload

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BTN_BROWSE = new System.Windows.Forms.Button();
+            this.BTN_MAKEFILE = new System.Windows.Forms.Button();
+            this.LBL_STATUS = new System.Windows.Forms.Label();
+            this.LBL_FILEPATH = new System.Windows.Forms.Label();
+            this.LBL_SAVINGPATH = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTN_BROWSE
+            // 
+            this.BTN_BROWSE.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BTN_BROWSE.Location = new System.Drawing.Point(401, 26);
+            this.BTN_BROWSE.Name = "BTN_BROWSE";
+            this.BTN_BROWSE.Size = new System.Drawing.Size(86, 26);
+            this.BTN_BROWSE.TabIndex = 0;
+            this.BTN_BROWSE.Text = "選擇檔案";
+            this.BTN_BROWSE.UseVisualStyleBackColor = true;
+            this.BTN_BROWSE.Click += new System.EventHandler(this.BTN_BROWSE_Click);
+            this.BTN_BROWSE.MouseLeave += new System.EventHandler(this.mouse_leaving);
+            // 
+            // BTN_MAKEFILE
+            // 
+            this.BTN_MAKEFILE.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BTN_MAKEFILE.Location = new System.Drawing.Point(401, 67);
+            this.BTN_MAKEFILE.Name = "BTN_MAKEFILE";
+            this.BTN_MAKEFILE.Size = new System.Drawing.Size(86, 26);
+            this.BTN_MAKEFILE.TabIndex = 1;
+            this.BTN_MAKEFILE.Text = "加/解密";
+            this.BTN_MAKEFILE.UseVisualStyleBackColor = true;
+            this.BTN_MAKEFILE.Click += new System.EventHandler(this.BTN_MAKEFILE_Click);
+            this.BTN_MAKEFILE.MouseLeave += new System.EventHandler(this.mouse_leaving);
+            // 
+            // LBL_STATUS
+            // 
+            this.LBL_STATUS.AutoSize = true;
+            this.LBL_STATUS.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LBL_STATUS.Location = new System.Drawing.Point(1, 143);
+            this.LBL_STATUS.Name = "LBL_STATUS";
+            this.LBL_STATUS.Size = new System.Drawing.Size(44, 17);
+            this.LBL_STATUS.TabIndex = 2;
+            this.LBL_STATUS.Text = "label1";
+            this.LBL_STATUS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LBL_FILEPATH
+            // 
+            this.LBL_FILEPATH.AutoSize = true;
+            this.LBL_FILEPATH.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LBL_FILEPATH.Location = new System.Drawing.Point(31, 29);
+            this.LBL_FILEPATH.Name = "LBL_FILEPATH";
+            this.LBL_FILEPATH.Size = new System.Drawing.Size(139, 15);
+            this.LBL_FILEPATH.TabIndex = 3;
+            this.LBL_FILEPATH.Text = "點擊右側按鈕以選擇檔案";
+            this.LBL_FILEPATH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LBL_SAVINGPATH
+            // 
+            this.LBL_SAVINGPATH.AutoSize = true;
+            this.LBL_SAVINGPATH.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LBL_SAVINGPATH.Location = new System.Drawing.Point(31, 70);
+            this.LBL_SAVINGPATH.Name = "LBL_SAVINGPATH";
+            this.LBL_SAVINGPATH.Size = new System.Drawing.Size(67, 16);
+            this.LBL_SAVINGPATH.TabIndex = 4;
+            this.LBL_SAVINGPATH.Text = "儲存路徑：";
+            this.LBL_SAVINGPATH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(519, 160);
+            this.Controls.Add(this.LBL_SAVINGPATH);
+            this.Controls.Add(this.LBL_FILEPATH);
+            this.Controls.Add(this.LBL_STATUS);
+            this.Controls.Add(this.BTN_MAKEFILE);
+            this.Controls.Add(this.BTN_BROWSE);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "檔案保護";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_BROWSE;
+        private System.Windows.Forms.Button BTN_MAKEFILE;
+        private System.Windows.Forms.Label LBL_STATUS;
+        private System.Windows.Forms.Label LBL_FILEPATH;
+        private System.Windows.Forms.Label LBL_SAVINGPATH;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static Aes AesMan = Aes.Create();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LBL_STATUS.Text = "請先選擇要處理的檔案。";
+            LBL_SAVINGPATH.Text += "G:\\codes\\測試區";
+            //AesMan.Key = Encoding.UTF8.GetBytes("今天天氣不是很好，我來加個密吧！");
+            //AesMan.IV = Encoding.UTF8.GetBytes("昨日死今日生，明日如何今日增aa");
+            byte[] mykey = {
+                0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+                0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
+            };
+            byte[] myIV = {
+                0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
+            };
+            AesMan.Key = mykey;
+            AesMan.IV = myIV;
+        }
+
+        private void BTN_BROWSE_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofDlg = new OpenFileDialog())
+            {
+                ofDlg.InitialDirectory = "G:\\codes\\測試區";
+                //string filterString = "txt檔|*.txt | 圖片檔|*.jpg;*.jpeg;*.png;*.gif | MS系列檔|*.docx;*.xlsx;*.pptx | MS系列檔(舊)|*.doc;*.xls;*.ppt | 加密檔|*.cryp | 所有檔案|*.*";
+                //ofDlg.Filter = filterString;
+                //ofDlg.FilterIndex = 1;
+                if (ofDlg.ShowDialog() == DialogResult.OK)
+                    LBL_FILEPATH.Text = ofDlg.FileName;
+            }
+        }
+
+        private void BTN_MAKEFILE_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(LBL_FILEPATH.Text))
+            {
+                byte[] cipherText;
+                string plainText;
+                string fileName = LBL_FILEPATH.Text.Split("\\")[^1];
+                if (fileName.Split(".")[^1] == "cryp")
+                {
+                    // 以bytes為單位讀取加密檔
+                    cipherText = File.ReadAllBytes(LBL_FILEPATH.Text);
+                    // 解密
+                    plainText = Decryption(cipherText);
+                    // 存檔
+                    using (StreamWriter outputFile = new StreamWriter("G:\\codes\\測試區\\" + fileName[0..^5])) {
+                        outputFile.Write(plainText);
+                    }
+
+                    LBL_STATUS.Text = "解密完成。";
+                }
+                else
+                {
+                    // 以字串形式讀取被加密檔
+                    using (StreamReader inputFile = new StreamReader(LBL_FILEPATH.Text)) {
+                        plainText = inputFile.ReadToEnd();
+                    }
+                    // 加密
+                    cipherText = Encryption(plainText);
+                    // 存檔
+                    File.WriteAllBytes("G:\\codes\\測試區\\" + fileName + ".cryp", cipherText);
+
+                    LBL_STATUS.Text = "加密完成。";
+                }
+            }
+            else
+                MessageBox.Show("請先選擇要加/解密的檔案。", "操作錯誤");
+        }
+
+        private void mouse_leaving(object sender, EventArgs e)
+        {
+            LBL_STATUS.Text = "就緒。";
+        }
+
+        private byte[] Encryption(string plainText)
+        {
+            byte[] encrypted;
+            ICryptoTransform Encryptor = AesMan.CreateEncryptor();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, Encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(plainText);
+                    }
+                    encrypted = ms.ToArray();
+                }
+            }
+            return encrypted;
+        }
+
+        private string Decryption(byte[] cipherText)
+        {
+            string decrypted;
+            ICryptoTransform Decryptor = AesMan.CreateDecryptor();
+
+            using (MemoryStream ms = new MemoryStream(cipherText))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, Decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sr = new StreamReader(cs))
+                    {
+                        decrypted = sr.ReadToEnd();
+                    }
+                }
+            }
+            return decrypted;
+        }
+
+    }
+}
